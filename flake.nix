512d5d301fff61c7eeb7df7daeee364beaaa3d66
Moises Eskinazi <moiesk@gmail.com>
2026-08-14 21:31:01 -0400

    build(deps): bump lavish-axi to 0.1.46

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
     };
 
     lavish-axi = {
-      url = "github:kunchenguid/lavish-axi/lavish-axi-v0.1.45";
+      url = "github:kunchenguid/lavish-axi/lavish-axi-v0.1.46";
       flake = false;
     };
 
