de1cccf974caf32d42888443f7a8d4767d639d7b
Moises Eskinazi <moiesk@gmail.com>
2026-09-02 09:03:22 -0400

    build(deps): roll treehouse pin back to v2.2.0 (v2.3.0 ships failing tests)

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     nix-homebrew.inputs.brew-src.follows = "brew-src";
 
     treehouse = {
-      url = "github:kunchenguid/treehouse/v2.3.0";
+      url = "github:kunchenguid/treehouse/v2.2.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
