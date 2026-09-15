8f7ea81177c7ddd40cebaa179ee8fe3bb4be4c51
Moises Eskinazi <moiesk@gmail.com>
2026-09-14 20:48:18 -0400

    chore(agent-tools): pin lavish-axi 0.1.67

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
     };
 
     lavish-axi = {
-      url = "github:kunchenguid/lavish-axi/lavish-axi-v0.1.65";
+      url = "github:kunchenguid/lavish-axi/lavish-axi-v0.1.67";
       flake = false;
     };
 
